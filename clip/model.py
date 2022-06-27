cf1278d4c19dfc892e3b8f94e8b2c5db4d3f3410
pmj951030 <61683867+pmj951030@users.noreply.github.com>
2022-06-28 00:20:43 +0900

    Add files via upload

--- clip/model.py
+++ clip/model.py
@@ -398,7 +398,6 @@ def convert_weights(model: nn.Module):
 
 
 def build_model(state_dict: dict):
-    global state_dict
     vit = "visual.proj" in state_dict
 
     if vit:
@@ -430,9 +429,9 @@ def build_model(state_dict: dict):
         context_length, vocab_size, transformer_width, transformer_heads, transformer_layers
     )
 
-    for key in ["input_resolution", "context_length", "vocab_size"]:   ##mj
-        if key in state_dict:  ##mj
-            del state_dict[key]   #mj
+    for key in ["input_resolution", "context_length", "vocab_size"]:
+        if key in state_dict:
+            del state_dict[key]
 
     convert_weights(model)
     model.load_state_dict(state_dict)
