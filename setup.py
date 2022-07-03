dba039a9d26fde428cd26ef4f89ca540616301c7
pmj951030 <61683867+pmj951030@users.noreply.github.com>
2022-07-03 22:44:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="clip",
     py_modules=["clip"],
-    version="1.139",
+    version="1.14",
     description="",
     author="OpenAI",
     packages=find_packages(exclude=["tests*"]),
